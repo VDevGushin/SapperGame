952e8ff213b59247c1923574d2ac2d728f29b5f8
Vladislav Gus <vdev@vdev.local>
2017-02-10 22:19:46 +0300

    Initial Commit

---
+++ Sapper/GameSettings.swift
@@ -0,0 +1,14 @@
+//
+//  GameSettings.swift
+//  Sapper
+//
+//  Created by Vladislav Gushin on 10/02/2017.
+//  Copyright © 2017 VladislavGushin. All rights reserved.
+//
+
+import Foundation
+
+struct GameSettings{
+    let numColumnsAndRows: Int
+    let numberOfBombs : Int
+}
