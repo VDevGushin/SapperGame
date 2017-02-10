4fbade8f05a3633f708efe9b10c8bcf387f46486
Vladislav Gus <vdev@vdev.local>
2017-02-11 00:02:19 +0300

    add view controlller add settings

---
+++ Sapper/VDevGameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  VDevGameViewController.swift
+//  Sapper
+//
+//  Created by Vladislav Gushin on 10/02/2017.
+//  Copyright © 2017 VladislavGushin. All rights reserved.
+//
+
+import UIKit
+
+class VDevGameViewController: UIViewController {
+
+    var gameSettings : GameSettings?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Sapper/ViewController.swift
+++ Sapper/ViewController.swift
@@ -10,18 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RowsAndColumnsTF: UILabel!
+    @IBOutlet weak var BombsCountTF: UILabel!
+    @IBOutlet weak var RowsAndColumnsSlider: UISlider!
+    @IBOutlet weak var BombsCountSlider: UISlider!
 
-    @IBOutlet weak var RowsAndColumnsTextField: UITextField!
-    @IBOutlet weak var BombsCountTextField: UITextField!
+    var rowsAndColumnsCount = 0
+    var maxBombsCount = 0
+    var bombsCout = 0
+
+    @IBAction func rowsAndColumnsSliderValueChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value / 1) * 1
+        sender.value = roundedValue
+        rowsAndColumnsCount = Int(roundedValue)
+        updateRowsColumnsCountNumber()
+        updateMaxBombsCount()
+    }
+
+    @IBAction func bombsCountValueChanged(_ sender: UISlider) {
+        let roundedValue = round(sender.value / 1) * 1
+        sender.value = roundedValue
+        bombsCout = Int(roundedValue)
+        updateCurrentBombsNumber()
+    }
+
+    @IBAction func startGameAction(_ sender: UIButton) {
+        let gameSettings = GameSettings(numColumnsAndRows: rowsAndColumnsCount, numberOfBombs: bombsCout)
+        print(gameSettings)
+        performSegue(withIdentifier: "BeginGameSegue", sender: gameSettings)
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "BeginGameSegue" {
+            if let colorViewController = segue.destination as? VDevGameViewController {
+                colorViewController.gameSettings = sender as? GameSettings
+            }
+        }
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initFields()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    private func initFields(){
+        rowsAndColumnsCount = Int((RowsAndColumnsSlider.value))
+        bombsCout = Int(BombsCountSlider.value)
+        updateRowsColumnsCountNumber()
+        updateCurrentBombsNumber()
+        updateMaxBombsCount()
+    }
+
+    private func updateRowsColumnsCountNumber(){
+        RowsAndColumnsTF.text = String(rowsAndColumnsCount)
+    }
+
+    private func updateCurrentBombsNumber(){
+         BombsCountTF.text =  String(bombsCout)
+    }
+
+    private func updateMaxBombsCount(){
+        maxBombsCount = (rowsAndColumnsCount * rowsAndColumnsCount) - 1
+        BombsCountSlider.maximumValue = Float(maxBombsCount)
     }
 }
 
