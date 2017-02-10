b127a980963950d3a65c90a77e8ba0080865a1d5
Vladislav Gus <vdev@vdev.local>
2017-02-10 23:00:37 +0300

    Add UI interface

--- Sapper/ViewController.swift
+++ Sapper/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var RowsAndColumnsTextField: UITextField!
+    @IBOutlet weak var BombsCountTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
